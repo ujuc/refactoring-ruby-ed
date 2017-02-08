1d9bea0efd1ffbcdf1ce788646b1d505edad6bc3
Sungjin Knag <ujuc@ujuc.kr>
2017-02-09 00:29:12 +0900

    1차 나누기

--- ch1/customer.rb
+++ ch1/customer.rb
@@ -20,19 +20,7 @@ class Customer
     result = "고객 #{@name}의 대여 기록\n"
 
     @rentals.each do |element|
-      this_amount = 0
-
-      # 영화 종류별 내용을 각각 구함
-      case element.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-      when Movie::NEW_RELEASE
-        this_amount += element.days_rented * 3
-      when Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
-      end
+      this_amount = amount_for(element)
 
       # 적립 포인트를 더함
       frequent_renter_points += 1
@@ -50,6 +38,23 @@ class Customer
     # 푸터 행 추가
     result += "대여료는 #{total_amount}입니다.\n"
     result += "적립 포인트는 #{frequent_renter_points} 입니다."
+    result
+  end
+
+  def amount_for(rental)
+    # 영화 종류별 내용을 각각 구함
+    result = 0
+
+    case rental.movie.price_code
+      when Movie::REGULAR
+        result += 2
+        result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
+      when Movie::NEW_RELEASE
+        result += rental.days_rented * 3
+      when Movie::CHILDRENS
+        result += 1.5
+        result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
+    end
     result
   end
 end
