d078ff1e6fd0e6e525f41e6641f5ebbf0f50fa1b
Sungjin Knag <ujuc@ujuc.kr>
2017-02-09 00:32:36 +0900

    다른 곳으로 이동

--- ch1/customer.rb
+++ ch1/customer.rb
@@ -42,19 +42,6 @@ class Customer
   end
 
   def amount_for(rental)
-    # 영화 종류별 내용을 각각 구함
-    result = 0
-
-    case rental.movie.price_code
-      when Movie::REGULAR
-        result += 2
-        result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-      when Movie::NEW_RELEASE
-        result += rental.days_rented * 3
-      when Movie::CHILDRENS
-        result += 1.5
-        result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-    end
-    result
+    rental.change
   end
 end
--- ch1/rental.rb
+++ ch1/rental.rb
@@ -1,3 +1,5 @@
+require '../ch1/movie'
+
 # 영화를 대여중인 고객을 나타낸다
 class Rental
   attr_reader :movie, :days_rented
@@ -5,5 +7,22 @@ class Rental
   def initialize(movie, days_rented)
     @movie = movie
     @days_rented = days_rented
+  end
+
+  def change
+    # 영화 종류별 내용을 각각 구함
+    result = 0
+
+    case rental.movie.price_code
+      when Movie::REGULAR
+        result += 2
+        result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
+      when Movie::NEW_RELEASE
+        result += rental.days_rented * 3
+      when Movie::CHILDRENS
+        result += 1.5
+        result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
+    end
+    result
   end
 end
