43c164383f2f6b4f83cb3ee196255a15d7cc3366
Sungjin Knag <ujuc@ujuc.kr>
2017-02-09 00:48:09 +0900

    26p

--- ch1/customer.rb
+++ ch1/customer.rb
@@ -15,7 +15,6 @@ class Customer
   end
 
   def statement
-    total_amount = 0
     frequent_renter_points = 0
     result = "고객 #{@name}의 대여 기록\n"
 
@@ -25,12 +24,23 @@ class Customer
 
       # 이번 대여의 계산 결과를 표시
       result += "\t" + element.movie.title + "\t" + element.change.to_s + "\n"
-      total_amount += element.change
     end
 
     # 푸터 행 추가
-    result += "대여료는 #{total_amount}입니다.\n"
+    result += "대여료는 #{total_charge}입니다.\n"
     result += "적립 포인트는 #{frequent_renter_points} 입니다."
+    result
+  end
+
+  private
+
+  def total_charge
+    result = 0
+
+    @rentals.each do |element|
+      result += element.charge
+    end
+
     result
   end
 end
