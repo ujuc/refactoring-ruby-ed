af71aa1282036cdba03eea031a48c926009c7472
Sungjin Knag <ujuc@ujuc.kr>
2017-02-09 01:14:35 +0900

    43p

--- ch1/movie.rb
+++ ch1/movie.rb
@@ -9,6 +9,11 @@ class Movie
 
   def price_code=(value)
     @price_code = value
+    @price = case price_code
+       when REGULAR: RegularPrice.new
+       when NEW_RELEASE: NewReleasePrice.new
+       when CHILDRENS: ChildrensPrice.new
+     end
   end
 
   def initialize(title, the_price_code)
@@ -37,7 +42,7 @@ class Movie
   end
 end
 
-class ReqularPrice
+class RegularPrice
 
 end
 
