59d172b3afe56e09c42b6ed1139c48043be870cf
Sungjin Knag <ujuc@ujuc.kr>
2017-02-09 00:38:24 +0900

    19p

--- ch1/customer.rb
+++ ch1/customer.rb
@@ -20,8 +20,6 @@ class Customer
     result = "고객 #{@name}의 대여 기록\n"
 
     @rentals.each do |element|
-      this_amount = element.change
-
       # 적립 포인트를 더함
       frequent_renter_points += 1
 
@@ -31,8 +29,8 @@ class Customer
       end
 
       # 이번 대여의 계산 결과를 표시
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
+      result += "\t" + element.movie.title + "\t" + element.change.to_s + "\n"
+      total_amount += element.change
     end
 
     # 푸터 행 추가
